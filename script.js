45cb7156966193036436ce036c032c7a5f28a35d
Vasil Ivanov <iv.vasil09@gmail.com>
2019-09-09 21:55:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,303 @@
+$(document).ready(function() {
+  /*
+
+  $("#btnlgin").click(function() {
+    $("#lgin").hide();
+  });
+
+  $("#btnlgin").click(function() {
+    $("#wlcm").show();
+  });
+
+  $("#btnlgin").click(function() {
+    $("#welc").append(
+      "Welcome, " +
+        $("#nm").val() +
+        "! <br> Let us make your home more colorfull."
+    );
+  });
+*/
+
+  // BUTTONS COLORS
+
+  $("#btn1").click(function() {
+    $("#bdrm1").css("background-color", "#DD4132");
+  });
+
+  $("#btn2").click(function() {
+    $("#bdrm1").css("background-color", "#9E1030");
+  });
+
+  $("#btn3").click(function() {
+    $("#bdrm1").css("background-color", "#FE840E");
+  });
+
+  $("#btn4").click(function() {
+    $("#bdrm1").css("background-color", "#FF6F61");
+  });
+
+  $("#btn5").click(function() {
+    $("#bdrm1").css("background-color", "#C62168");
+  });
+
+  $("#btn6").click(function() {
+    $("#bdrm1").css("background-color", "#8D9440");
+  });
+
+  $("#btn7").click(function() {
+    $("#bdrm1").css("background-color", "#FFD662");
+  });
+
+  $("#btn8").click(function() {
+    $("#bdrm1").css("background-color", "#00539C");
+  });
+
+  $("#btn9").click(function() {
+    $("#bdrm1").css("background-color", "#755139");
+  });
+
+  $("#btn10").click(function() {
+    $("#bdrm1").css("background-color", "#D69C2F");
+  });
+
+  $("#btn11").click(function() {
+    $("#bdrm1").css("background-color", "#616247");
+  });
+
+  $("#btn12").click(function() {
+    $("#bdrm1").css("background-color", "#E8B5CE");
+  });
+
+  $("#btn13").click(function() {
+    $("#bdrm1").css("background-color", "#D2C29D");
+  });
+
+  $("#btn14").click(function() {
+    $("#bdrm1").css("background-color", "#343148");
+  });
+
+  $("#btn15").click(function() {
+    $("#bdrm1").css("background-color", "#F0EAD6");
+  });
+
+  $("#btn16").click(function() {
+    $("#bdrm1").css("background-color", "#615550");
+  });
+
+  $("#btn17").click(function() {
+    $("#ktchn1").css("background-color", "#DD4132");
+  });
+
+  $("#btn18").click(function() {
+    $("#ktchn1").css("background-color", "#9E1030");
+  });
+
+  $("#btn19").click(function() {
+    $("#ktchn1").css("background-color", "#FE840E");
+  });
+
+  $("#btn20").click(function() {
+    $("#ktchn1").css("background-color", "#FF6F61");
+  });
+
+  $("#btn21").click(function() {
+    $("#ktchn1").css("background-color", "#C62168");
+  });
+
+  $("#btn22").click(function() {
+    $("#ktchn1").css("background-color", "#8D9440");
+  });
+
+  $("#btn23").click(function() {
+    $("#ktchn1").css("background-color", "#FFD662");
+  });
+
+  $("#btn24").click(function() {
+    $("#ktchn1").css("background-color", "#00539C");
+  });
+
+  $("#btn25").click(function() {
+    $("#ktchn1").css("background-color", "#755139");
+  });
+
+  $("#btn26").click(function() {
+    $("#ktchn1").css("background-color", "#D69C2F");
+  });
+
+  $("#btn27").click(function() {
+    $("#ktchn1").css("background-color", "#616247");
+  });
+
+  $("#btn28").click(function() {
+    $("#ktchn1").css("background-color", "#E8B5CE");
+  });
+
+  $("#btn29").click(function() {
+    $("#ktchn1").css("background-color", "#D2C29D");
+  });
+
+  $("#btn30").click(function() {
+    $("#ktchn1").css("background-color", "#343148");
+  });
+
+  $("#btn31").click(function() {
+    $("#ktchn1").css("background-color", "#F0EAD6");
+  });
+
+  $("#btn32").click(function() {
+    $("#ktchn1").css("background-color", "#615550");
+  });
+
+  $("#btn33").click(function() {
+    $("#tlt1").css("background-color", "#DD4132");
+  });
+
+  $("#btn34").click(function() {
+    $("#tlt1").css("background-color", "#9E1030");
+  });
+
+  $("#btn35").click(function() {
+    $("#tlt1").css("background-color", "#FE840E");
+  });
+
+  $("#btn36").click(function() {
+    $("#tlt1").css("background-color", "#FF6F61");
+  });
+
+  $("#btn37").click(function() {
+    $("#tlt1").css("background-color", "#C62168");
+  });
+
+  $("#btn38").click(function() {
+    $("#tlt1").css("background-color", "#8D9440");
+  });
+
+  $("#btn39").click(function() {
+    $("#tlt1").css("background-color", "#FFD662");
+  });
+
+  $("#btn40").click(function() {
+    $("#tlt1").css("background-color", "#00539C");
+  });
+
+  $("#btn41").click(function() {
+    $("#tlt1").css("background-color", "#755139");
+  });
+
+  $("#btn42").click(function() {
+    $("#tlt1").css("background-color", "#D69C2F");
+  });
+
+  $("#btn43").click(function() {
+    $("#tlt1").css("background-color", "#616247");
+  });
+
+  $("#btn44").click(function() {
+    $("#tlt1").css("background-color", "#E8B5CE");
+  });
+
+  $("#btn45").click(function() {
+    $("#tlt1").css("background-color", "#D2C29D");
+  });
+
+  $("#btn46").click(function() {
+    $("#tlt1").css("background-color", "#343148");
+  });
+
+  $("#btn47").click(function() {
+    $("#tlt1").css("background-color", "#F0EAD6");
+  });
+
+  $("#btn48").click(function() {
+    $("#tlt1").css("background-color", "#615550");
+  });
+
+  $("#btn49").click(function() {
+    $("#lvrm1").css("background-color", "#DD4132");
+  });
+
+  $("#btn50").click(function() {
+    $("#lvrm1").css("background-color", "#9E1030");
+  });
+
+  $("#btn51").click(function() {
+    $("#lvrm1").css("background-color", "#FE840E");
+  });
+
+  $("#btn52").click(function() {
+    $("#lvrm1").css("background-color", "#FF6F61");
+  });
+
+  $("#btn53").click(function() {
+    $("#lvrm1").css("background-color", "#C62168");
+  });
+
+  $("#btn54").click(function() {
+    $("#lvrm1").css("background-color", "#8D9440");
+  });
+
+  $("#btn55").click(function() {
+    $("#lvrm1").css("background-color", "#FFD662");
+  });
+
+  $("#btn56").click(function() {
+    $("#lvrm1").css("background-color", "#00539C");
+  });
+
+  $("#btn57").click(function() {
+    $("#lvrm1").css("background-color", "#755139");
+  });
+
+  $("#btn58").click(function() {
+    $("#lvrm1").css("background-color", "#D69C2F");
+  });
+
+  $("#btn59").click(function() {
+    $("#lvrm1").css("background-color", "#616247");
+  });
+
+  $("#btn60").click(function() {
+    $("#lvrm1").css("background-color", "#E8B5CE");
+  });
+
+  $("#btn61").click(function() {
+    $("#lvrm1").css("background-color", "#D2C29D");
+  });
+
+  $("#btn62").click(function() {
+    $("#lvrm1").css("background-color", "#343148");
+  });
+
+  $("#btn63").click(function() {
+    $("#lvrm1").css("background-color", "#F0EAD6");
+  });
+
+  $("#btn64").click(function() {
+    $("#lvrm1").css("background-color", "#615550");
+  });
+
+  /* BTNS SHOW/HIDE */
+  $("#bdrmbtn").click(function() {
+    $("#bdrm1").slideToggle("slow");
+  });
+
+  $("#ktchnbtn").click(function() {
+    $("#ktchn1").slideToggle("slow");
+  });
+
+  $("#tltbtn").click(function() {
+    $("#tlt1").slideToggle("slow");
+  });
+
+  $("#lvrmbtn").click(function() {
+    $("#lvrm1").slideToggle("slow");
+  });
+
+  $("#clrsbtndrp").click(function() {
+    $("#section21").slideToggle("fast");
+  });
+
+  $("#srvbtndrp").click(function() {
+    $("#section31").slideToggle("fast");
+  });
+});
